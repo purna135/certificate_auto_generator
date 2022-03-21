95922681c1f1c3a722860463fe0572b779d5f04a
Sourcery AI <>
2022-03-21 14:53:32 +0000

    'Refactored by Sourcery'

--- login/views.py
+++ login/views.py
@@ -6,47 +6,49 @@ from .models import student
 
 def loginview( request ):
     print ( request.method, request.FILES)
-    if request . method == 'POST' and len ( request.FILES) == 0 :
-            if request.POST['uid'] == 'litindia' and request.POST['password'] == 'litindia':
-                return render (request, 'askexcelfile.html')
-            else:
-                return render ( request, 'index.html', {'error': 'Invalid Login'})
-    elif request.method == 'POST' and len ( request.FILES) != 0 :
-            fileobj = open ('data.csv', 'wb')
+    if request . method == 'POST':
+        if len(request.FILES) == 0:
+            return (
+                render(request, 'askexcelfile.html')
+                if request.POST['uid'] == 'litindia'
+                and request.POST['password'] == 'litindia'
+                else render(request, 'index.html', {'error': 'Invalid Login'})
+            )
+
+        with open ('data.csv', 'wb') as fileobj:
             fileobj . write ( request.FILES ['studfile'] . read() )
-            fileobj . close ()
-            df = pd . read_csv ('data.csv')
-            i = 0
-            img_list = []
-            msg = []
-            while i < len (df):
-                name = df . loc [ i ] . Name
-                cource = df . loc [ i ] . Cource
-                reg = df . loc [ i ] . RegNo
-                mid = df . loc [ i ] . Mailid
-                if len ( student . objects .  filter ( name = name , cource = cource , regno = reg, mid = mid )) == 0:
-                    obj = student ( name = name , cource = cource , regno = reg, mid = mid )
-                    obj . save ()
-                    from PIL import Image
-                    from PIL import ImageFont
-                    from PIL import ImageDraw
+        df = pd . read_csv ('data.csv')
+        i = 0
+        img_list = []
+        msg = []
+        while i < len (df):
+            name = df . loc [ i ] . Name
+            cource = df . loc [ i ] . Cource
+            reg = df . loc [ i ] . RegNo
+            mid = df . loc [ i ] . Mailid
+            if len ( student . objects .  filter ( name = name , cource = cource , regno = reg, mid = mid )) == 0:
+                obj = student ( name = name , cource = cource , regno = reg, mid = mid )
+                obj . save ()
+                from PIL import Image
+                from PIL import ImageFont
+                from PIL import ImageDraw
 
-                    img = Image.open("login/static/lit.jpg")
-                    draw = ImageDraw.Draw(img)
-                    # font = ImageFont.truetype(<font-file>, <font-size>)
-                    font = ImageFont.truetype("./arial.ttf",25);
-                    # draw.text((x, y),"Sample Text",(r,g,b))
-                    draw.text((1035, 1720),name,(0,0,0),font=font)
-                    draw.text((1144, 2100),cource,(0,0,0),font=font)
+                img = Image.open("login/static/lit.jpg")
+                draw = ImageDraw.Draw(img)
+                # font = ImageFont.truetype(<font-file>, <font-size>)
+                font = ImageFont.truetype("./arial.ttf",25);
+                # draw.text((x, y),"Sample Text",(r,g,b))
+                draw.text((1035, 1720),name,(0,0,0),font=font)
+                draw.text((1144, 2100),cource,(0,0,0),font=font)
 
-                    img_name=name + cource+'.jpg'
-                    img.save(r'login\static\certificate\{}'.format(img_name))
-                    img_list . append ( img_name )
-                else :
-                    msg.append( name + ' Allready taken Certificate on ' + cource + ' having REGNo ' + str(reg))
-                i += 1
-            # if not msg :
-            #     msg = str ( img_list )
-                    
-            return render (request, 'display.html', {'img_list':img_list, 'msg':msg})
+                img_name=name + cource+'.jpg'
+                img.save(f'login\\static\\certificate\\{img_name}')
+                img_list . append ( img_name )
+            else:
+                msg.append( name + ' Allready taken Certificate on ' + cource + ' having REGNo ' + str(reg))
+            i += 1
+        # if not msg :
+        #     msg = str ( img_list )
+
+        return render (request, 'display.html', {'img_list':img_list, 'msg':msg})
     return render ( request, 'index.html')
